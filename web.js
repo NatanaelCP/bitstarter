53b5b21d90a26c5f3d2707613beb3075a5f168c7
Natanael Peranginangin <natanael.peranginangin@gmail.com>
2013-07-15 09:50:26 +0800

    update again to web.js

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
 var express = require('express');
-
+var fs = require('fs');
+var htmlfile = "index.html";
+ 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var fs = require('fs');
-  var infile = "index.html";
-  var content = fs.readFileSync(infile, "utf8");
-  response.send(content);
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
 var port = process.env.PORT || 5000;
